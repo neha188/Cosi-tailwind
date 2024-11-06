3fb1c5569563def97cbd6afe15b0fe11f6ee7301
neha <neha.sharma@snackmagic.com>
2024-11-06 11:27:40 +0530

    scroll components added

--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     "./src/app/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/components/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/components/ui/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
